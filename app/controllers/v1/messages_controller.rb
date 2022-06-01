b3ea28e791a2607185ebfd6424b656aeb02c16d9
uae <aniekaudo@yahoo.com>
2022-06-01 18:28:13 +0100

    :sparkles: Messages controller
    Created a new message method

--- app/controllers/v1/messages_controller.rb
+++ app/controllers/v1/messages_controller.rb
@@ -3,4 +3,14 @@ class V1::MessagesController < ApplicationController
     @message = Message.random_message
     render json: { greetings: @message }.to_json
   end
+
+  def create
+    @message = Message.new(title: title)
+
+    if @message.save 
+      render json: @message, status: :created, location: @message
+    else
+      render json: @message.errors, status: :unprocessable_entity
+    end
+  end
 end

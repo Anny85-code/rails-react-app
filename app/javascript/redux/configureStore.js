55478125ba6661361f604c7a042fc172eaa2fafc
uae <aniekaudo@yahoo.com>
2022-06-01 18:24:45 +0100

    :sparkles: Added seed messages
    Added seed message

--- app/javascript/redux/configureStore.js
+++ app/javascript/redux/configureStore.js
@@ -1,10 +1,10 @@
 import { legacy_createStore  as createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-import reducerGreeting from './greetings/greetingReducer'
+import greetingReducer from './greetings/greetingReducer'
 
 const rootReducer = combineReducers({
-    greetings: reducerGreeting
+    greetings: greetingReducer
 })
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Message.create([{id: 1, title: "Hello World", created_at: "2020-05-31T01:29:41.000Z", updated_at: "2020-05-31T01:29:41.000Z"}, {id: 2, title: "Hello rails", created_at: "2020-05-31T01:29:42.000Z", updated_at: "2020-05-31T01:29:44.000Z"}, {id: 3, title: "Hello react", created_at: "2020-05-31T01:29:45.000Z", updated_at: "2020-05-31T01:29:48.000Z"}, {id: 4, title: "Hello Bright", created_at: "2020-05-31T01:30:49.000Z", updated_at: "2020-05-31T01:31:51.000Z"}, {id: 5, title: "Hello Anny", created_at: "2020-05-31T01:31:52.000Z", updated_at: "2020-05-31T01:32:54.000Z"}])
